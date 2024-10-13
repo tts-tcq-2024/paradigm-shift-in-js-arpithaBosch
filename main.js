957f9cbcce239a04f5867eba57f7964342125ff6
arpithaP <163737012+arpithaBosch@users.noreply.github.com>
2024-10-13 15:12:32 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,23 +1,15 @@
 const { batteryIsOk } = require('./batteryManagement');
 
-/**
- * Default reporter for console logging.
- * 
- * @param {string} message - Message to log.
- */
+// Default reporter for console logging.
 function defaultReporter(message) {
     console.log(message);
 }
 
-/**
- * Configuration object for warnings and tolerances.
- * 
- * @type {Object}
- */
+//Configuration object for warnings and tolerances.
 const config = {
-    temperature: { tolerance: 0.05, enableWarning: true }, // Enable warning for temperature
-    soc: { tolerance: 0.05, enableWarning: true },         // Enable warning for SoC
-    chargeRate: { tolerance: 0.05, enableWarning: true }   // Enable warning for charge rate
+    temperature: { tolerance: 0.05, enableWarning: true },
+    soc: { tolerance: 0.05, enableWarning: true },
+    chargeRate: { tolerance: 0.05, enableWarning: true }
 };
 
 module.exports = { config, batteryIsOk, defaultReporter };
